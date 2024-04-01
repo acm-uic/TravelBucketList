d94c20af25ca6ac6b456476052ca85e4b1e1feba
cheetodustflori <150205115+cheetodustflori@users.noreply.github.com>
2024-04-01 08:33:24 -0500

    adding category view & views!

---
+++ SigIOSFinal/Details/CategoryView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryView.swift
+//  SigIOSFinal
+//
+//  Created by Quynh Tram on 3/10/24.
+//
+
+import SwiftUI
+
+struct CategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryView()
+}
