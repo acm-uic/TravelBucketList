b5d2cf9082163bac617fbeaada1ac424917f25a3
cheetodustflori <150205115+cheetodustflori@users.noreply.github.com>
2024-04-01 08:34:09 -0500

    adding categoryview

--- SigIOSFinal/Details/CategoryView.swift
+++ SigIOSFinal/Details/CategoryView.swift
@@ -8,11 +8,23 @@
 import SwiftUI
 
 struct CategoryView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+    var category:  Category
+        
+        var destinations: [Destination]{
+            return Destination.all.filter{$0.category == category.rawValue}
+        }
+        
+        var body: some View {
+            ScrollView{
+                DestinationsList(destinations:destinations)
+            }
+            .navigationTitle(category.rawValue)
+        }
+        
 }
 
-#Preview {
-    CategoryView()
+struct CategoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryView(category: Category.vacation)
+    }
 }
